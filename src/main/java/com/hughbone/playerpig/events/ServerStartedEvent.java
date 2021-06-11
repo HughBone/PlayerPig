01958ab9fa73b37be72c5142086d2f8c075ba8f4
HughBone <hughbone27@gmail.com>
2021-06-11 15:33:43 -0400

    this looks nicer

--- src/main/java/com/hughbone/playerpig/events/ServerStartedEvent.java
+++ src/main/java/com/hughbone/playerpig/events/ServerStartedEvent.java
@@ -3,9 +3,6 @@ package com.hughbone.playerpig.events;
 import com.hughbone.playerpig.piglist.LoadPigList;
 import com.hughbone.playerpig.piglist.PigList;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.scoreboard.AbstractTeam;
-import net.minecraft.scoreboard.Scoreboard;
-import net.minecraft.scoreboard.Team;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.world.ServerWorld;
@@ -17,33 +14,42 @@ public class ServerStartedEvent {
 
     private List<List<String>> unloadedPigList;
     private MinecraftServer server;
-    public static Team noCollision;
-    public static Scoreboard teamScoreboard;
 
-    private void loadUnloadChunk() {
-        try {
-            CommandManager cm = new CommandManager(CommandManager.RegistrationEnvironment.ALL);
-
-            for (List<String> unloadedPiggy : unloadedPigList) {
-                int posX = (int) Double.parseDouble(unloadedPiggy.get(0));
-                int posZ = (int) Double.parseDouble(unloadedPiggy.get(2));
-
-                // Temporarily load chunk in correct dimension so EntityLoadEvent adds pig to PigList
-                Iterable<ServerWorld> worlds = server.getWorlds();
-                for (ServerWorld sw : worlds) {
-                    String dimension = sw.getRegistryKey().getValue().toString();
-                    if (unloadedPiggy.get(3).equals(dimension)) {
-                        cm.getDispatcher().execute("execute in " + dimension + " run forceload add " + posX + " " + posZ, server.getCommandSource());
-                        Thread.sleep(100);
-                        cm.getDispatcher().execute("execute in " + dimension + " run forceload remove " + posX + " " + posZ, server.getCommandSource());
-                        break;
-                    }
-                }
+    public void init() {
+        ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
+            // Temporarily load chunks so that EntityLoadEvent can add pigs to PigList
+            List<List<String>> unloadedPigList = LoadPigList.getAllData();
+            if (!unloadedPigList.isEmpty()) {
+                this.unloadedPigList = unloadedPigList;
+                this.server = server;
+                new ServerStartedThread().start();
             }
-        } catch (Exception e) {}
+        });
     }
 
     public class ServerStartedThread extends Thread {
+        private void loadUnloadChunk() {
+            try {
+                CommandManager cm = new CommandManager(CommandManager.RegistrationEnvironment.ALL);
+
+                for (List<String> unloadedPiggy : unloadedPigList) {
+                    int posX = (int) Double.parseDouble(unloadedPiggy.get(0));
+                    int posZ = (int) Double.parseDouble(unloadedPiggy.get(2));
+
+                    // Temporarily load chunk in correct dimension so EntityLoadEvent adds pig to PigList
+                    Iterable<ServerWorld> worlds = server.getWorlds();
+                    for (ServerWorld sw : worlds) {
+                        String dimension = sw.getRegistryKey().getValue().toString();
+                        if (unloadedPiggy.get(3).equals(dimension)) {
+                            cm.getDispatcher().execute("execute in " + dimension + " run forceload add " + posX + " " + posZ, server.getCommandSource());
+                            Thread.sleep(100);
+                            cm.getDispatcher().execute("execute in " + dimension + " run forceload remove " + posX + " " + posZ, server.getCommandSource());
+                            break;
+                        }
+                    }
+                }
+            } catch (Exception e) {}
+        }
 
         public void run() {
             for (int i = 6; i > 0 && unloadedPigList.size() != PigList.getList().size(); i--) {
@@ -59,27 +65,6 @@ public class ServerStartedEvent {
             }
             LoadPigList.deleteAll();
         }
-
-    }
-
-    public void init() {
-        ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
-            // No collisions for players logging in (so they don't get pushed by pigs)
-            teamScoreboard = server.getScoreboard();
-            try {
-                noCollision = teamScoreboard.addTeam("nocollision");
-            } catch (Exception e){}
-            noCollision = teamScoreboard.getTeam("nocollision");
-            noCollision.setCollisionRule(AbstractTeam.CollisionRule.NEVER);
-
-            // Temporarily load chunks so that EntityLoadEvent can add pigs to PigList
-            List<List<String>> unloadedPigList = LoadPigList.getAllData();
-            if (!unloadedPigList.isEmpty()) {
-                this.unloadedPigList = unloadedPigList;
-                this.server = server;
-                new ServerStartedThread().start();
-            }
-        });
     }
 
 }
