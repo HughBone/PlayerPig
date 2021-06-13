c36c699d5412aa65944a7728a87d590ca5e56b54
HughBone <hughbone27@gmail.com>
2021-06-13 14:17:12 -0400

    add removal message

--- src/main/java/com/hughbone/playerpig/commands/PigremoveallCommand.java
+++ src/main/java/com/hughbone/playerpig/commands/PigremoveallCommand.java
@@ -1,5 +1,6 @@
 package com.hughbone.playerpig.commands;
 
+import com.hughbone.playerpig.PlayerPigExt;
 import com.hughbone.playerpig.piglist.LoadPigList;
 import com.hughbone.playerpig.util.PPUtil;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
@@ -9,6 +10,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
+import net.minecraft.world.GameRules;
 
 import java.util.List;
 
@@ -38,13 +40,19 @@ public class PigremoveallCommand {
                                 for (ServerWorld sw : worlds) {
                                     String dimension = sw.getRegistryKey().getValue().toString();
                                     if (unloadedPiggy.get(3).equals(dimension)) {
+                                        boolean sendCommandFB = player.getServer().getGameRules().get(GameRules.SEND_COMMAND_FEEDBACK).get(); // original value
+                                        player.getServer().getGameRules().get(GameRules.SEND_COMMAND_FEEDBACK).set(false, player.getServer()); // set to false
+
                                         cm.getDispatcher().execute("execute in " + dimension + " run forceload add " + posX + " " + posZ, player.getServer().getCommandSource());
                                         Thread.sleep(250);
                                         for (PigEntity piggy : PPUtil.getPigList()) {
+                                            player.sendMessage(new LiteralText("Removing " + ((PlayerPigExt) piggy).getPlayerName() + ".."), false);
                                             piggy.remove(Entity.RemovalReason.KILLED);
                                         }
                                         Thread.sleep(50);
                                         cm.getDispatcher().execute("execute in " + dimension + " run forceload remove " + posX + " " + posZ, player.getServer().getCommandSource());
+
+                                        player.getServer().getGameRules().get(GameRules.SEND_COMMAND_FEEDBACK).set(sendCommandFB, player.getServer()); // reset to original
                                         break;
                                     }
                                 }
@@ -54,11 +62,11 @@ public class PigremoveallCommand {
                     ctx.getSource().sendFeedback(new LiteralText("[PlayerPig] All PlayerPigs removed."), false);
                     return 1;
                 })));
+
+                PPUtil.getPigList().clear(); // Clear all elements from the list
             }
         };
 
         thread.start();
-
-
     }
 }
