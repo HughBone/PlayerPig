411cb37c7c27e4a3617ffb1077e8d9cd0b0babfa
HughBone <hughbone27@gmail.com>
2021-06-26 14:13:06 -0400

    merge two pigentity mixins

--- src/main/java/com/hughbone/playerpig/mixin/PlayerPigMixin.java
+++ src/main/java/com/hughbone/playerpig/mixin/PlayerPigMixin.java
@@ -1,20 +1,36 @@
 package com.hughbone.playerpig.mixin;
 
+import com.hughbone.playerpig.util.PPUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import com.hughbone.playerpig.PlayerPigExt;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // Adds playerPig, playerName, and playerUUID tags
 @Mixin(PigEntity.class)
-public abstract class PlayerPigMixin implements PlayerPigExt {
+public abstract class PlayerPigMixin extends LivingEntity implements PlayerPigExt  {
 
     private boolean playerPig = false;
     private String matchingPlayerName = "";
     private String matchingPlayerUUID = "";
+
+    protected PlayerPigMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("RETURN"))
     public void writeCustomDataToTag (NbtCompound nbt, CallbackInfo ci) {
@@ -61,7 +77,50 @@ public abstract class PlayerPigMixin implements PlayerPigExt {
         matchingPlayerUUID = uuid;
     }
 
+    // Stop turning into zombified piglin
+    @Inject(method = "onStruckByLightning", at = @At("HEAD"), cancellable = true)
+    public void lightningStrike(ServerWorld world, LightningEntity lightning, CallbackInfo ci) {
+        if (playerPig) {
+            ci.cancel();
+        }
+    }
 
+    // Stop from dying
+    @Inject(method = "getDeathSound", at = @At("HEAD"))
+    public void stopPPDeath(CallbackInfoReturnable<SoundEvent> cir) {
+        // Allow pig to die from /kill command
+        if (playerPig) {
+            if (String.valueOf(lastDamageTaken).equals("3.4028235E38")) {
+                this.remove(RemovalReason.DISCARDED);
+                PPUtil.getPigList().remove((PigEntity)(Object)this);
+                PPUtil.removeFile(matchingPlayerUUID);
+            }
+            else {
+                this.setHealth(20F);
+                this.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 2147483647, 5, false, false));
+            }
+        }
+    }
 
+    // Stop from falling into the void (teleports to 0, 100, 0)
+    @Inject(method = "getHurtSound", at = @At("HEAD"))
+    public void stopPPVoidDamage(DamageSource source, CallbackInfoReturnable<SoundEvent> cir) {
+        try {
+            if (playerPig) {
+                if (source.isOutOfWorld()) {
+                    this.updatePosition(0, 100, 0);
+                    this.updateTrackedPosition(0, 100, 0);
+                    this.setHealth(20F);
+                    this.setVelocity(Vec3d.ZERO);
+
+                    while (this.isInsideWall()) {
+                        double newY = this.getY() + 10;
+                        this.updatePosition(this.getX(), newY, this.getZ());
+                        this.updateTrackedPosition(this.getX(), newY, this.getZ());
+                    }
+                }
+            }
+        } catch (Exception e) {}
+    }
 
 }
