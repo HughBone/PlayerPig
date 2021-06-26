c6b9330d32ee9318cf7d02b3e512d3668e603dd8
HughBone <hughbone27@gmail.com>
2021-06-26 14:19:47 -0400

    improve piglist command

--- src/main/java/com/hughbone/playerpig/commands/PiglistCommand.java
+++ src/main/java/com/hughbone/playerpig/commands/PiglistCommand.java
@@ -3,72 +3,96 @@ package com.hughbone.playerpig.commands;
 import com.hughbone.playerpig.PlayerPigExt;
 import com.hughbone.playerpig.piglist.LoadPigList;
 import com.hughbone.playerpig.util.PPUtil;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.server.command.*;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 
 import java.util.List;
 
 public class PiglistCommand {
 
+    private static void sendMessage(CommandContext<ServerCommandSource> ctx, int x, int y, int z, String world, String playerName) {
+        try {
+
+            CommandManager cm = new CommandManager(CommandManager.RegistrationEnvironment.ALL);
+
+            String command = "tellraw " + ctx.getSource().getPlayer().getEntityName() +
+                    " {\"text\":\"" +
+                    "(" + playerName + ": " + x + ", " + y + ", " + z + ", " + world + ")" +
+                    "\",\"color\":\"blue\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"" + "/execute in " +
+                    world +
+                    " run tp @p " +
+                    x + " " + y + " " + z +
+                    "\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"" +
+                    playerName + "\"}]}}";
+
+            cm.getDispatcher().execute(command, ctx.getSource().getMinecraftServer().getCommandSource());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void init() {
         // List all player pigs in memory
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(CommandManager.literal("piglist").executes(ctx -> {
 
-
             if (ctx.getSource().getPlayer().hasPermissionLevel(4) || ctx.getSource().getPlayer().getEntityName().equals("HughBone")) { // HughBone is here for debugging
                 Thread thread = new Thread() {
                     public void run() {
-                        CommandManager cm = new CommandManager(CommandManager.RegistrationEnvironment.ALL);
-                        // Load all in data folder
-                        for (List<String> unloadedPiggy : LoadPigList.getAllData()) {
-                            int posX = (int) Double.parseDouble(unloadedPiggy.get(0));
-                            int posZ = (int) Double.parseDouble(unloadedPiggy.get(2));
+                        boolean playerPigsFound = false;
 
-                            // Temporarily load chunk in correct dimension so EntityLoadEvent adds pig to PigList
-                            Iterable<ServerWorld> worlds = ctx.getSource().getMinecraftServer().getWorlds();
-                            for (ServerWorld sw : worlds) {
-                                String dimension = sw.getRegistryKey().getValue().toString();
-                                if (unloadedPiggy.get(3).equals(dimension)) {
-                                    PPUtil.loadPPDataChunks(ctx.getSource().getMinecraftServer(), dimension, posX, posZ);
-                                    break;
+                        outer:
+                        for (List<String> unloadedPiggy : LoadPigList.getAllData()) {
+                            for (PigEntity pigInList : PPUtil.getPigList()) {
+                                if (((PlayerPigExt) pigInList).getPlayerUUID().equals(unloadedPiggy.get(4))) {
+                                    continue outer;
                                 }
                             }
-                        }
-                        PPUtil.deleteAllFiles();
 
-                        for (PigEntity pigInList: PPUtil.getPigList()) {
-                            String textLine = "tellraw $target {\"text\":\"$text\",\"color\":\"blue\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/execute in $world run tp @p $posX $posY $posZ\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":[{\"text\":\"$hovertext\"}]}}";
-
+                            int posX = 0;
+                            int posY = 0;
+                            int posZ = 0;
+                            String dimension = null;
+                            String playerName = "Unloaded_PlayerPig";
                             try {
-                                textLine = textLine.replace("$target", (ctx.getSource().getPlayer().getEntityName()));
-                                textLine = textLine.replace("$text", "(" + ((PlayerPigExt) pigInList).getPlayerName() + ": " + pigInList.getBlockPos().getX() + ", " + pigInList.getBlockPos().getY() + ", " + pigInList.getBlockPos().getZ() + ", " + pigInList.world.getRegistryKey().getValue() + ")");
-                                textLine = textLine.replace("$hovertext", ((PlayerPigExt) pigInList).getPlayerName());
-                                textLine = textLine.replace("$world", pigInList.world.getRegistryKey().getValue().toString());
-                                textLine = textLine.replace("$posX", String.valueOf(pigInList.getBlockPos().getX()));
-                                textLine = textLine.replace("$posY", String.valueOf(pigInList.getBlockPos().getY()));
-                                textLine = textLine.replace("$posZ", String.valueOf(pigInList.getBlockPos().getZ()));
-                                cm.getDispatcher().execute(textLine, ctx.getSource().getMinecraftServer().getCommandSource());
+                                posX = (int) Double.parseDouble(unloadedPiggy.get(0));
+                                posY = (int) Double.parseDouble(unloadedPiggy.get(1));
+                                posZ = (int) Double.parseDouble(unloadedPiggy.get(2));
+                                dimension = unloadedPiggy.get(3);
+                                playerName = unloadedPiggy.get(5);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
 
-                            } catch (CommandSyntaxException e) {}
+                            sendMessage(ctx, posX, posY, posZ, dimension, playerName);
+                            playerPigsFound = true;
+                        }
 
+                        for (PigEntity pigInList : PPUtil.getPigList()) {
+                            sendMessage(ctx, pigInList.getBlockX(), pigInList.getBlockY(), pigInList.getBlockZ(), pigInList.world.getRegistryKey().getValue().toString(), ((PlayerPigExt) pigInList).getPlayerName());
+                            playerPigsFound = true;
                         }
 
                         // Success messages
-                        if (PPUtil.getPigList().isEmpty()) {
-                            ctx.getSource().sendFeedback(new LiteralText("[PlayerPig] No player pigs found."), false);
+                        if (!playerPigsFound) {
+                            try {
+                                ctx.getSource().getPlayer().sendMessage(new LiteralText("[PlayerPig] No player pigs found."), false);
+                            } catch (CommandSyntaxException e) {
+                                e.printStackTrace();
+                            }
                         }
 
                     }
                 };
-                thread.start();
-            }
 
-            else {
-                ctx.getSource().sendFeedback(new LiteralText("[PlayerPig] You need OP to use this command."), false);
+                thread.start();
+            } else {
+                ctx.getSource().getPlayer().sendMessage(new LiteralText("[PlayerPig] You need OP to use this command."), false);
             }
             return 1;
         })));
